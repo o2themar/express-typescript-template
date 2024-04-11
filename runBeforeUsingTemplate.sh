d72b63a3b240da4543d2be6d9b4d93c9f543ad96
Omar Ali <oali@socialgist.com>
2024-04-11 16:28:01 -0400

    Added script to setup template with project name and description.

---
+++ runBeforeUsingTemplate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "Please provide a project name: "
+read PROJECT_NAME
+
+echo "Please provide a project description: "
+read PROJECT_DESCRIPTION
+
+PACKAGE_JSON_FILE="package.json"
+README_FILE="README.md"
+
+sed -i '' "s/PROJECT_NAME_HERE/$PROJECT_NAME/g" $README_FILE
+sed -i '' "s/PROJECT_NAME_HERE/$PROJECT_NAME/g" $PACKAGE_JSON_FILE
+sed -i '' "s/PROJECT_DESCRIPTION_HERE/$PROJECT_DESCRIPTION/g" $PACKAGE_JSON_FILE
+
+echo "The project is all setup. You can now delete this script."
