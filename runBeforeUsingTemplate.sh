ac36f853d7d1c3f7f608e725a80524294476ff22
Omar Ali <oali@socialgist.com>
2024-04-12 11:41:18 -0400

    Added steps to script for setting up CI/CD.

--- runBeforeUsingTemplate.sh
+++ runBeforeUsingTemplate.sh
@@ -16,4 +16,8 @@ sed -i '' "s/PROJECT_DESCRIPTION_HERE/$PROJECT_DESCRIPTION/g" $PACKAGE_JSON_FILE
 cp .env.template .env
 
 echo "Please rename the top level project folder to '${PROJECT_NAME}'"
+echo "To get github actions working the following steps need to be done:"
+echo "1. Create a github classic token with write:packages, read:packages, and delete:packages permissions."
+echo "2. Create in project Settings > Security > Secrets & varaibles > Actions a token with name GH_Token and with the value from step 1."
+echo "3. Make sure in project Settings > code & automation > Actions > General > Work Flow pemissions is set to read & write."
 echo "The project is all setup. You can now delete this script."
