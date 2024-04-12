57928f04ae4f78c072ebbcef8ead700039199bc7
Omar Ali <oali@socialgist.com>
2024-04-12 06:30:25 -0400

    Copying env template for local dev use in setup script.

--- runBeforeUsingTemplate.sh
+++ runBeforeUsingTemplate.sh
@@ -13,5 +13,7 @@ sed -i '' "s/PROJECT_NAME_HERE/$PROJECT_NAME/g" $README_FILE
 sed -i '' "s/PROJECT_NAME_HERE/$PROJECT_NAME/g" $PACKAGE_JSON_FILE
 sed -i '' "s/PROJECT_DESCRIPTION_HERE/$PROJECT_DESCRIPTION/g" $PACKAGE_JSON_FILE
 
+cp .env.template .env
+
 echo "Please rename the top level project folder to '${PROJECT_NAME}'"
 echo "The project is all setup. You can now delete this script."
